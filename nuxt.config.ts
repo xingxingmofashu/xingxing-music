96855ed0bcd1982fe516d1dec980e7c0cb0a8fa3
xingxingmofashu <xingxingmofashu@outlook.com>
2023-11-21 17:35:27 +0800

    fix:ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 const isDev = process.env.NODE_ENV === 'development'
 const isElectron = process.env.IS_ELECTRON === 'true'
 export default defineNuxtConfig({
+  ssr:false
   /* app: {
     head: {
       htmlAttrs: {
